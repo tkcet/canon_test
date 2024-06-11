1bef53e17266c29671070b84a3933ae5fb9fe910
student <student@WIN-I1TIVKUOSNP>
2024-06-11 14:24:36 +0900

    mika1st

--- src/p01/Sample01.java
+++ src/p01/Sample01.java
@@ -5,6 +5,7 @@ public class Sample01 {
 	public static void main(String[] args) {
 		System.out.println("木村");
 		System.out.println(24);
+		
 	}
 
 }
---
+++ src/p19/Mika.java
@@ -0,0 +1,10 @@
+package p19;
+
+public class Mika {
+	public static void main(String[] args) {
+		String error = "エラーは無いです";
+		System.out.println("(^。^)y-.。o○");
+		System.out.println(error);
+		
+	}
+}
